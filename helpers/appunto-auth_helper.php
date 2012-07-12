9e852c11d7e1c016d5870341d7896e513b72cfe5
Bill Clinton <bclinton@appunto.net>
2012-07-12 00:42:20 -0400

    continuing to flesh out helpers in an attempt to remove controllers completely from spark

--- helpers/appunto-auth_helper.php
+++ helpers/appunto-auth_helper.php
@@ -9,6 +9,113 @@
 /*
  *
  */
+if ( ! function_exists('appunto_auth_application'))
+{
+	function appunto_auth_application($height="600px", $width="100%", $additional_css='')
+	{
+		$CI =& get_instance();
+
+		$base_url = base_url();
+
+		$html = <<<APP
+<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/loading_mask.css" />
+<div id="loading-mask"></div>
+<div id="loading">
+  <div class="loading-indicator">
+    <span id="loading-msg">Loading styles and images...</span>
+  </div>
+</div>
+<div id='script-includes'>
+
+	<!-- ExtJS style sheets -->
+	<link rel="stylesheet" type="text/css" href="http://cdn.sencha.io/ext-4.0.7-gpl/resources/css/ext-all.css">
+
+	<!-- Application style sheets -->
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_app.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_login.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_curvy.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_branding.css" /> 
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_icons.css" />
+
+	<!-- set base url, display type and height -->
+	<script type="text/javascript">
+		var appunto_auth_base_url = '{$base_url}index.php/',	
+			appunto_auth_display_type = 'container',
+			appunto_auth_height = '$height';
+	</script>
+
+	<!-- ExtJS library -->
+	<!-- <script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext.js"></script> -->
+	<script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext-dev.js"></script>
+	<!-- <script type="text/javascript" src="http://extjs.cachefly.net/ext-4.0.7-gpl/ext-dev.js"></script> -->
+	<!-- <script type="text/javascript" src="extjs/ext.js"></script> -->
+
+	<!-- Application requirements -->
+	<script type="text/javascript" src="{$base_url}app.js"></script>
+
+</div>
+<div id="appunto-auth-app-div" style="width:$width;height:$height;$additional_css">
+</div>
+APP;
+
+	return $html;
+	}
+}
+/*
+ *
+ */
+if ( ! function_exists('appunto_auth_application_viewport'))
+{
+	function appunto_auth_application_viewport()
+	{
+		$CI =& get_instance();
+
+		$base_url = base_url();
+
+		$html = <<<APP
+<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/loading_mask.css" />
+<div id="loading-mask"></div>
+<div id="loading">
+  <div class="loading-indicator">
+    <span id="loading-msg">Loading styles and images...</span>
+  </div>
+</div>
+<div id='script-includes'>
+
+	<!-- ExtJS style sheets -->
+	<link rel="stylesheet" type="text/css" href="http://cdn.sencha.io/ext-4.0.7-gpl/resources/css/ext-all.css">
+
+	<!-- Application style sheets -->
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_app.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_login.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_curvy.css" />
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_branding.css" /> 
+	<link rel="stylesheet" type="text/css" href="{$base_url}resources/appunto-auth/css/appunto_auth_icons.css" />
+
+	<!-- set a base_url -->
+	<script type="text/javascript">var appunto_auth_base_url = '{$base_url}index.php/';</script>
+
+	<!-- set display type -->
+	<script type="text/javascript">var appunto_auth_display_type = 'viewport';</script>
+
+	<!-- ExtJS library -->
+	<script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Sencha ExtJS...';</script>
+	<!-- <script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext.js"></script> -->
+	<script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext-dev.js"></script>
+	<!-- <script type="text/javascript" src="http://extjs.cachefly.net/ext-4.0.7-gpl/ext-dev.js"></script> -->
+	<!-- <script type="text/javascript" src="extjs/ext.js"></script> -->
+
+	<!-- Application requirements -->
+	<script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Application...';</script>
+	<script type="text/javascript" src="{$base_url}app.js"></script>
+
+</div>
+APP;
+
+	return $html;
+	}
+}
+
 if ( ! function_exists('login_box'))
 {
 	function login_box($return=false, $css = 'login-form-box')
@@ -127,7 +234,7 @@ if ( ! function_exists('login_header'))
 		$html = <<<LOGINBOX
 $form_open
 <div class="login-wrap">
-	<div class="login-form">
+	<div class="login-form-header">
 		$message_div
 		<div class="login-form-errors">$validation_errors</div>
 		<div class="login-form-label">$label_username</div>
