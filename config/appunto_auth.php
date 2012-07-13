c20c42c3613d273cc613ef603b03010f1b7be57b
Bill Clinton <bclinton@appunto.net>
2012-07-12 21:07:20 -0400

    added options

--- config/appunto_auth.php
+++ config/appunto_auth.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['site_name'] = 'My Application';
+$config['site_name'] = 'Appunto Auth';
 $config['admin_name'] = 'Administrator';
 $config['admin_email'] = 'admin@website.com';
 
@@ -12,13 +12,28 @@ $config['appunto_auth_version'] = '0.0.1';
 /**
  * Behavior for private paths without permissions.
  *
- * if true, paths marked as private that don't have a permission set will be visible 
- * to any logged in user.
+ * default: false
+ *
+ * If true, paths marked as private that don't have a permission set will be visible 
+ * to any logged in user.  This may be useful if you have numerous URIs that should
+ * be accessile to all logged in users, and you don't want to add a permission to each
+ * one.
  * 
- * if false, paths marked as private that don't have a permission set will be essentially
+ * If false, paths marked as private that don't have a permission set will be essentially
  * inaccessible until the admin assigns a permission to that path.   
  */
 $config['allow_private_without_permission'] = false;
+
+/**
+ * Ext JS URL
+ *
+ * This is the URL of or the path to the Ext JS Library.  By default, we are loading 
+ * it from the Sencha CDN, but you can choose a different CDN or download Ext JS and 
+ * put it in your application's path. Make sure you include the trailing slash.
+ */
+//$config['extjs_url'] = '/my-extjs-location/';
+//$config['extjs_url'] = 'http://extjs.cachefly.net/ext-4.0.7-gpl/';
+$config['extjs_url'] = 'http://cdn.sencha.io/ext-4.0.7-gpl/';
 
 /**
  * PHPPass Settings
@@ -34,6 +49,20 @@ $config['hash_use_portable'] = false;
 /**
  * Database table prefix
  *
- * a prefix that will be added to all table names.
+ * A prefix that will be added to all table names. Leave this blank if your database 
+ * doesn't add a prefix to your tables.
  */
 $config['db_table_prefix'] = '';
+
+/**
+ * Dev Mode
+ *
+ * default: false
+ *
+ * Do not change this from false unless you are working on the interface's source
+ * code.  If you do not have the source files in the correct directory the interface
+ * will not work at all.  If you do have the source files in the correct location,
+ * the interface will render very slowly, expecially if you are loading the ExtJS
+ * library from an external address, so do not leave this set to true in production,
+ */
+$config['appunto_dev_mode'] = false;
