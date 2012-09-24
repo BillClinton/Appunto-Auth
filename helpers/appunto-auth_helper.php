3f26a896727f3796ee28fb1fd305ec861d6da7c2
Bill Clinton <bclinton@appunto.net>
2012-09-24 01:25:58 -0400

    switch to compiled app file

--- helpers/appunto-auth_helper.php
+++ helpers/appunto-auth_helper.php
@@ -56,7 +56,8 @@ if ( ! function_exists('appunto_auth_application'))
 
 	<!-- Application requirements -->
 	<script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Application...';</script>
-	<script type="text/javascript" src="{$base_url}app.js"></script>
+	<!-- <script type="text/javascript" src="{$base_url}app.js"></script> -->
+	<script type="text/javascript" src="{$base_url}resources/appunto-auth/app-all.js"></script>
 
 </div>
 <div id="appunto-auth-app-div" style="width:$width;height:$height;$additional_css">
@@ -115,7 +116,8 @@ if ( ! function_exists('appunto_auth_application_viewport'))
 
 	<!-- Application requirements -->
 	<script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Application...';</script>
-	<script type="text/javascript" src="{$base_url}app.js"></script>
+	<!-- <script type="text/javascript" src="{$base_url}app.js"></script> -->
+	<script type="text/javascript" src="{$base_url}resources/appunto-auth/app-all.js"></script>
 
 </div>
 APP;
