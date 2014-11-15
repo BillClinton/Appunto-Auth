667087ae0137ff42fe2341cfe29abd2692068745
BillClinton <bclinton@appunto.net>
2014-11-14 19:17:31 -0500

    adding Portuguese translation of CodeIgniter language elements from CIBr/CodeIgniter-Portuguese-BR

---
+++ application/language/portuguese-br/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$lang['required']	= "O campo %s &eacute; obrigat&oacute;rio.";
+$lang['isset']	= "O campo %s deve ter um valor definido.";
+$lang['valid_email']	= "O campo %s deve conter um endere&ccedil;o de e-mail v&aacute;lido.";
+$lang['valid_emails']	= "O campo %s deve apenas endere&ccedil;os de e-mail v&aacute;lidos.";
+$lang['valid_url']	= "O campo %s deve conter uma URL v&aacute;lida.";
+$lang['valid_ip']	= "O campo %s deve conter um IP v&aacute;lido.";
+$lang['min_length']	= "O campo %s deve conter pelo menos %s caracteres.";
+$lang['max_length']	= "O campo %s n&atilde;o deve conter mais de %s caracteres.";
+$lang['exact_length']	= "O campo %s deve conter exatamente %s caracteres.";
+$lang['alpha']	= "O campo %s deve conter apenas letras.";
+$lang['alpha_numeric']	= "O campo %s deve conter apenas caracteres alfanum&eacute;ricos.";
+$lang['alpha_dash']	= "O campo %s deve conter apenas caracteres alfanum&eacute;ricos, sublinhados e tra&ccedil;os.";
+$lang['numeric']	= "O campo %s deve conter apenas n&uacute;meros.";
+$lang['is_numeric']	= "O campo %s deve conter apenas caracteres num&eacute;ricos.";
+$lang['integer']	= "O campo %s deve conter um n&uacute;mero inteiro.";
+$lang['regex_match']	= "O campo %s n&atilde;o est&aacute; no formato correto.";
+$lang['matches']	= "O campo %s deve ser igual ao campo %s.";
+$lang['is_unique'] = "O campo %s deve conter um valor &uacute;nico.";
+$lang['is_natural']	= "O campo %s deve conter apenas n&uacute;meros positivos.";
+$lang['is_natural_no_zero']	= "O campo %s deve conter apenas n&uacute;meros maiores que zero.";
+$lang['decimal']	= "O campo %s deve conter um n&uacute;mero decimal.";
+$lang['less_than']	= "O campo %s deve conter um n&uacute;mero menor que %s.";
+$lang['greater_than']	= "O campo %s deve conter um n&uacute;mero maior que %s.";
+
+/* End of file form_validation_lang.php */
+/* Location: ./application/language/portuguese-br/form_validation_lang.php */
