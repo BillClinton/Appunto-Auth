383e752934dc7867d1daed7cccd8d2189c6f15ff
BillClinton <bclinton@appunto.net>
2015-01-05 22:13:42 -0500

    removed short tags

--- application/views/appunto-auth/error.php
+++ application/views/appunto-auth/error.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title><?php if (isset($site_name)) echo $site_name ?><?php if (isset($auth_message)) echo ' - '.$auth_message ?></title>
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>resources/appunto-auth/css/login.css" /> 
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/appunto-auth/css/login.css" /> 
 </head>
 <body>
 
--- application/views/appunto-auth/login.php
+++ application/views/appunto-auth/login.php
@@ -1,14 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>resources/appunto-auth/css/login.css" /> 
-
-    <title><? echo $site_name ?></title>
-
+    <title><?php if (isset($site_name)) echo $site_name ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/appunto-auth/css/login.css" /> 
 </head>
 <body>
-<? echo appunto_login_box($auth_message) ?>
+<?php echo appunto_login_box($auth_message) ?>
 </body>
 </html>
 
