6c154c5d0b3d0ccbb50d303709906590e8389d8d
bvr <bvrignaud@free.fr>
2015-01-14 11:35:43 +0400

    BUGFIX in the error view : miss php in  <? ?>

--- application/views/appunto-auth/error.php
+++ application/views/appunto-auth/error.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php if (isset($site_name)) echo $site_name ?><?php if (isset($auth_message)) echo ' - '.$auth_message ?></title>
+    <title>
+        <?php if (isset($site_name)) echo $site_name ?><?php if (isset($auth_message)) echo ' - '.$auth_message ?>
+    </title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/appunto-auth/css/login.css" /> 
 </head>
 <body>
@@ -9,7 +11,7 @@
 <div class="login-form-box">
 <?php if (isset($auth_message)) { ?>
 	<div class="login-form-auth-message"><?php echo $auth_message ?></div>
-<? } ?>
+<?php } ?>
 </div>
 
 </body>
