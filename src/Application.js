45e677f1d2a1a1267d95961a0c561aaeebce1f68
BillClinton <bclinton@appunto.net>
2015-01-17 14:27:45 -0500

    removed the Proxy requirement.  This is now being required in the model classes that use the proxy

--- src/Application.js
+++ src/Application.js
@@ -3,41 +3,38 @@
  * Ext.application(). This is the ideal place to handle application launch and initialization
  * details.
  */
-Ext.require('AppuntoAuth.lib.proxy.Codeigniter');
+//Ext.require('AppuntoAuth.lib.proxy.Codeigniter');
 Ext.require('AppuntoAuth.lib.util.CustomVTypes');
 Ext.require('AppuntoAuth.lib.util.FakeConsole');
 Ext.require('AppuntoAuth.lib.util.SearchField');
 Ext.require('AppuntoAuth.lib.util.KeepAlive');
 Ext.require('AppuntoAuth.lib.lang.Default');
 
-Ext.ClassManager.addNameAliasMappings({
-	'AppuntoAuth.lib.proxy.Codeigniter': ['proxy.ci']
-});
-
-Ext.define('AppuntoAuth.Application', {
+Ext.define('AppuntoAuth.Application', 
+{
     extend: 'Ext.app.Application',
     
     name: 'AppuntoAuth',
 
-	enableQuickTips : true,
+    enableQuickTips : true,
 
     controllers: [
-		'Navigation',
-		'Status',
-		'Users',
-		'Roles',
-		'Permissions',
-		'Paths',
-		'Sessions'
+        'Navigation',
+        'Status',
+        'Users',
+        'Roles',
+        'Permissions',
+        'Paths',
+        'Sessions'
     ],
 
     launch: function () 
-	{
-		Ext.tip.QuickTipManager.init();
-		AppuntoAuth.lib.util.CustomVTypes.apply();
-		AppuntoAuth.lib.util.FakeConsole.init();
-		AppuntoAuth.lib.lang.Default.init();
+    {
+        Ext.tip.QuickTipManager.init();
+        AppuntoAuth.lib.util.CustomVTypes.apply();
+        AppuntoAuth.lib.util.FakeConsole.init();
+        AppuntoAuth.lib.lang.Default.init();
 
-		AppuntoAuth.lib.util.KeepAlive.init(admin_keepalive);
+        AppuntoAuth.lib.util.KeepAlive.init(admin_keepalive);
     }
 });
