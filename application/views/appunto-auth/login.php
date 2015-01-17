a391d2987672439e38cba65752252732190f2094
BillClinton <bclinton@appunto.net>
2015-01-17 14:32:51 -0500

    minor formatting

--- application/views/appunto-auth/login.php
+++ application/views/appunto-auth/login.php
@@ -2,12 +2,9 @@
 <html>
 <head>
     <title><?php if (isset($site_name)) echo $site_name ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/appunto-auth/css/login.css" /> 
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/appunto-auth/css/login.css" /> 
 </head>
 <body>
 <?php echo appunto_login_box($auth_message) ?>
 </body>
 </html>
-
-
-
